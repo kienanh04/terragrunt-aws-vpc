1fc66a344e091279359a84c7c83a06092726623e
kienanh04 <kienanh04@gmail.com>
2021-05-27 17:10:48 +0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -47,15 +47,15 @@ variable "ssh_public_key" {
 }
 
 # Endpoint vars:
-variable "enable_dynamodb_endpoint" {
-  description = "Should be true if you want to provision a DynamoDB endpoint to the VPC"
-  default     = false
-}
+##variable "enable_dynamodb_endpoint" {
+##  description = "Should be true if you want to provision a DynamoDB endpoint to the VPC"
+##  default     = false
+##}
 
-variable "enable_s3_endpoint" {
-  description = "Should be true if you want to provision an S3 endpoint to the VPC"
-  default     = true
-}
+##variable "enable_s3_endpoint" {
+##  description = "Should be true if you want to provision an S3 endpoint to the VPC"
+##  default     = true
+##}
 
 variable "tags" {
   default = {}
